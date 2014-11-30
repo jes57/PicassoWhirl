205c2097be50e150433f0f049aee0b94bdb28a9f
jes57 <jasonstephens1084@gmail.com>
2014-11-30 15:38:02 -0500

    Initial commit with all Dr. Liszkas files

--- app/src/main/java/com/garufa/picassowhirl/MainActivity.java
+++ app/src/main/java/com/garufa/picassowhirl/MainActivity.java
@@ -1,39 +1,22 @@
-package com.garufa.picassowhirl;
+package com.basementkittehs.www.simplegraphics;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.opengl.GLSurfaceView;
+
 
 
 public class MainActivity extends Activity {
 
+    private GLSurfaceView mGLView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
     }
 }
